116fbb29d2b91e53d68c9a77d92ceeb49013ed7a
Oluwatuyi Abiola <abiolaoluwatuyi@gmail.com>
2022-08-16 14:00:27 +0100

    post to port

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.post("/failure", function(req, res){
 
 
 
-app.listen(process.env.POST || 3000, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server is live at port 3000");
 })
 
